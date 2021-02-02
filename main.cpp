5452a30d2795783bef08bca0a0f82bb5e7441773
phong.dang <phong12a5@gmail.com>
2021-02-01 20:22:35 -0800

    Unlock chilkat

--- main.cpp
+++ main.cpp
@@ -4,25 +4,35 @@
 
 #include <QCoreApplication>
 #include <AppMain.h>
+#include <CkGlobal.h>
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// Main() ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+CkGlobal glob;
 
-/**
- * @paragraph This function executes the application
- * @brief main()
- * @param int intArguments
- * @param char* charArguments[]
- * @return int
- */
+bool unlockChilkat();
+
 int main(int intArguments, char* charArguments[])
 {
         // Initializet the application
         QCoreApplication qcaGeoData(intArguments, charArguments);
-        // Instatiate the CGI mapper
+        // Unlock chilkat
+        unlockChilkat();
+
         AppMain myapp;
         myapp.handleRequest();
 
         qcaGeoData.exit();
+}
+
+bool unlockChilkat() {
+    bool success_global = glob.UnlockBundle("VONGTH.CB4082020_9kru5rnD5R2h");
+    if (success_global != true) {
+        return false;
+    }
+
+    int status = glob.get_UnlockStatus();
+    if (status == 2) {
+        return true;
+    } else {
+        return false;
+    }
 }
---
+++ release/upload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#sshpass -p '.J6qDU%dL,%j]RTC' scp -r * root@95.179.130.172:/var/www/cgi-bin
+sshpass -p '.J6qDU%dL,%j]RTC' scp autofarmer.api root@95.179.130.172:/var/www/cgi-bin
